355633b52ca21e5f67102dde1a13279ae8eef221
class02 <class02@Class02T01>
2021-04-12 18:16:12 +0900

    day01 commit

---
+++ coffee/src/com/increpas/coffee/controller/Resrc.java
@@ -0,0 +1,35 @@
+package com.increpas.coffee.controller;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+
+@WebServlet({"/css/*", "/js/*", "/img/*"})
+public class Resrc extends HttpServlet {
+	
+	public void service(HttpServletRequest req, HttpServletResponse resp) {
+		String url = req.getRequestURI();
+		System.out.println("url : " + url);
+		// ==> 요청 파일 경로 추출한다. 
+		/*
+			/css/w3.css 또는 /js/jquery-3.6.0.min.js
+		 */
+		url = url.substring(url.indexOf("/", 1));
+		
+		// 요청 루트 경로...
+		// <== /css || /js || /img
+		String spath = url.substring(0, url.indexOf("/", 1));
+		
+		url = url.substring(url.indexOf(spath + "/"));
+		
+		String view = "/WEB-INF/resources" + url;
+		
+		// 응답을 해준다.
+		RequestDispatcher rd = req.getRequestDispatcher(view);
+		try {
+			rd.forward(req, resp);
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
