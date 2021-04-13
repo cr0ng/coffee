f24d7b1e6924878968f9b438bb296a2bd12f29a1
class02 <class02@Class02T01>
2021-04-13 18:10:01 +0900

    jsp02 last commit

--- coffee/src/com/increpas/coffee/controller/Resrc.java
+++ coffee/src/com/increpas/coffee/controller/Resrc.java
@@ -8,24 +8,17 @@ import javax.servlet.annotation.*;
 public class Resrc extends HttpServlet {
 	
 	public void service(HttpServletRequest req, HttpServletResponse resp) {
-		String url = req.getRequestURI(); // 도메인 이후의 요청 내용 추출
-		System.out.println("url : " + url);
-		// ==> 요청 파일 경로 추출한다. 
+		String url = req.getRequestURI(); 
 		/*
-			/css/w3.css 또는 /js/jquery-3.6.0.min.js
+			w3.css 파일을 요청하는 경우
+				url	- /cafe/css/w3.css
+				
+			url에서 /cafe 이후부분을 추출해서 문서의 경로를 만들어주면 된다.
 		 */
+		
 		url = url.substring(url.indexOf("/", 1));
 		
-		// 요청 루트 경로...
-		// <== /css || /js || /img
-		String spath = url.substring(0, url.indexOf("/", 1));
-		
-		url = url.substring(url.indexOf(spath + "/"));
-		
-		String view = "/WEB-INF/resources" + url;
-		
-		// 응답을 해준다.
-		RequestDispatcher rd = req.getRequestDispatcher(view);
+		RequestDispatcher rd = req.getRequestDispatcher("/WEB-INF/resources" + url);
 		try {
 			rd.forward(req, resp);
 		} catch(Exception e) {
