1ae452b9990b08dba38096f3543a7cae1af2ace6
class02 <class02@Class02T01>
2021-04-14 14:55:26 +0900

    jsp03 mid commit

---
+++ coffee/src/test/Test01.java
@@ -0,0 +1,13 @@
+package test;
+
+public class Test01 {
+	String name = "김기은";
+	
+	
+	public Test01() {
+		int no = 10;
+		System.out.println("no : " + no);
+		/* 작업 진행 */
+		/* int no = 10; */
+	}
+}
